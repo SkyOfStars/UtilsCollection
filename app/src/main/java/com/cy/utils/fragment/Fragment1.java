3a25e17c487cde0b8b7b8b95ae945296f51b9265
cy <1165590578@qq.com>
2019-11-20 17:07:25 +0800

    添加网络监听

--- app/src/main/java/com/cy/utils/fragment/Fragment1.java
+++ app/src/main/java/com/cy/utils/fragment/Fragment1.java
@@ -7,15 +7,36 @@ import androidx.fragment.app.Fragment;
 
 import com.cy.utils.R;
 import com.cy.utils.fragment.base.BaseLazyFragment;
+import com.cy.utils.net.NetWatchdog;
 
 /**
  * created by cy on 2019/11/20 0020.
  */
 public class Fragment1 extends BaseLazyFragment {
 
+    private NetWatchdog mNetWatchdog;
+
     public static Fragment getInstance() {
         Fragment fragment = new Fragment1();
         return fragment;
+    }
+
+    /**
+     * 初始化网络监听
+     */
+    private void initNetWatchdog() {
+        mNetWatchdog = new NetWatchdog(getActivity());
+        mNetWatchdog.startWatch();
+
+        mNetWatchdog.setNetConnectedListener(new NetWatchdog.NetConnectedListener() {
+            @Override
+            public void onReNetConnected(boolean isReconnect) {
+            }
+
+            @Override
+            public void onNetUnConnected() {
+            }
+        });
     }
 
     @Override
@@ -35,6 +56,13 @@ public class Fragment1 extends BaseLazyFragment {
 
     @Override
     public void initListener() {
+        //
+        initNetWatchdog();
+    }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mNetWatchdog.stopWatch();
     }
 }
