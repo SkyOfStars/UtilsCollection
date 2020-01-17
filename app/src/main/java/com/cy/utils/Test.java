a50e27dba15e305e7206afe375044442fcc94d3f
cy <1165590578@qq.com>
2020-01-17 09:50:40 +0800

    添加图片处理工具类

--- app/src/main/java/com/cy/utils/Test.java
+++ app/src/main/java/com/cy/utils/Test.java
@@ -10,9 +10,10 @@ import java.util.List;
 /**
  * created by cy on 2019/11/27 0027.
  */
-public class Test {
+public final class Test {
     private static final String TAG = "Test";
-    public static  void  main(String[] args){
-        System.out.println("main: --time="+DateUtil.getDateByFormat("2019-12-21 12:34:46",DateUtil.dateFormatYMDHMS));
+
+    public static void main(String[] args) {
+        System.out.println("main: --time=" + DateUtil.getDateByFormat("2019-12-21 12:34:46", DateUtil.dateFormatYMDHMS));
     }
 }
--- app/src/main/java/com/cy/utils/common/AppUtil.java
+++ app/src/main/java/com/cy/utils/common/AppUtil.java
@@ -1,6 +1,9 @@
 package com.cy.utils.common;
 
+import android.app.Activity;
+import android.app.ActivityManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -8,14 +11,31 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.inputmethod.InputMethodManager;
 
+import com.cy.utils.bean.ProcessInfo;
+import com.cy.utils.bean.PsRow;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileReader;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * created by cy on 2019/11/20 0020.
  */
 public final class AppUtil {
+    private static final String TAG = "AppUtil";
+
     /**
      * 获取应用程序名称
      */
@@ -119,15 +139,259 @@ public final class AppUtil {
      */
     public static DisplayMetrics getDisplayMetrics(Context context) {
         Resources mResources;
-        if (context == null){
+        if (context == null) {
             mResources = Resources.getSystem();
 
-        }else{
+        } else {
             mResources = context.getResources();
         }
         //DisplayMetrics{density=1.5, width=480, height=854, scaledDensity=1.5, xdpi=160.421, ydpi=159.497}
         //DisplayMetrics{density=2.0, width=720, height=1280, scaledDensity=2.0, xdpi=160.42105, ydpi=160.15764}
         DisplayMetrics mDisplayMetrics = mResources.getDisplayMetrics();
         return mDisplayMetrics;
+    }
+
+    /**
+     * 描述：打开并安装文件.
+     *
+     * @param context the context
+     * @param file    apk文件路径
+     */
+    public static void installApk(Context context, File file) {
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setAction(android.content.Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.fromFile(file),
+                "application/vnd.android.package-archive");
+        context.startActivity(intent);
+    }
+
+    /**
+     * 描述：卸载程序.
+     *
+     * @param context     the context
+     * @param packageName 包名
+     */
+    public static void uninstallApk(Context context, String packageName) {
+        Intent intent = new Intent(Intent.ACTION_DELETE);
+        Uri packageURI = Uri.parse("package:" + packageName);
+        intent.setData(packageURI);
+        context.startActivity(intent);
+    }
+
+
+    /**
+     * 用来判断服务是否运行.
+     *
+     * @param context   the context
+     * @param className 判断的服务名字 "com.xxx.xx..XXXService"
+     * @return true 在运行 false 不在运行
+     */
+    public static boolean isServiceRunning(Context context, String className) {
+        boolean isRunning = false;
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> servicesList = activityManager.getRunningServices(Integer.MAX_VALUE);
+        Iterator<ActivityManager.RunningServiceInfo> l = servicesList.iterator();
+        while (l.hasNext()) {
+            ActivityManager.RunningServiceInfo si = (ActivityManager.RunningServiceInfo) l.next();
+            if (className.equals(si.service.getClassName())) {
+                isRunning = true;
+            }
+        }
+        return isRunning;
+    }
+
+    /**
+     * 停止服务.
+     *
+     * @param context   the context
+     * @param className the class name
+     * @return true, if successful
+     */
+    public static boolean stopRunningService(Context context, String className) {
+        Intent intent_service = null;
+        boolean ret = false;
+        try {
+            intent_service = new Intent(context, Class.forName(className));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (intent_service != null) {
+            ret = context.stopService(intent_service);
+        }
+        return ret;
+    }
+
+    /**
+     * Gets the number of cores available in this device, across all processors.
+     * Requires: Ability to peruse the filesystem at "/sys/devices/system/cpu"
+     *
+     * @return The number of cores, or 1 if failed to get result
+     */
+    public static int getNumCores() {
+        try {
+            //Get directory containing CPU info
+            File dir = new File("/sys/devices/system/cpu/");
+            //Filter to only list the devices we care about
+            File[] files = dir.listFiles(new FileFilter() {
+
+                @Override
+                public boolean accept(File pathname) {
+                    //Check if filename is "cpu", followed by a single digit number
+                    if (Pattern.matches("cpu[0-9]", pathname.getName())) {
+                        return true;
+                    }
+                    return false;
+                }
+
+            });
+            //Return the number of cores (virtual CPU devices)
+            return files.length;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 1;
+        }
+    }
+
+    /**
+     * Gps是否打开
+     * 需要<uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />权限
+     *
+     * @param context the context
+     * @return true, if is gps enabled
+     */
+    public static boolean isGpsEnabled(Context context) {
+        LocationManager lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        return lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    /**
+     * 打开键盘.
+     *
+     * @param context the context
+     */
+    public static void showSoftInput(Context context) {
+        InputMethodManager inputMethodManager = (InputMethodManager) context
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+
+    /**
+     * 关闭键盘事件.
+     *
+     * @param context the context
+     */
+    public static void closeSoftInput(Context context) {
+        InputMethodManager inputMethodManager = (InputMethodManager) context
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (inputMethodManager != null && ((Activity) context).getCurrentFocus() != null) {
+            inputMethodManager.hideSoftInputFromWindow(((Activity) context).getCurrentFocus()
+                    .getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
+
+    public static ApplicationInfo getApplicationInfo(Context context, String processName) {
+        if (processName == null) {
+            return null;
+        }
+
+        PackageManager packageManager = context.getApplicationContext().getPackageManager();
+        List<ApplicationInfo> appList = packageManager.getInstalledApplications(PackageManager.GET_UNINSTALLED_PACKAGES);
+        for (ApplicationInfo appInfo : appList) {
+            if (processName.equals(appInfo.processName)) {
+                return appInfo;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 描述：kill进程.
+     *
+     * @param context
+     * @param pid
+     */
+    public static void killProcesses(Context context, int pid, String processName) {
+    	/*String cmd = "kill -9 "+pid;
+    	Process process = null;
+	    DataOutputStream os = null;
+    	try {
+			process = Runtime.getRuntime().exec("su");
+			os = new DataOutputStream(process.getOutputStream());
+			os.writeBytes(cmd + "\n");
+			os.writeBytes("exit\n");
+			os.flush();
+			process.waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	AbLogUtil.d(AbAppUtil.class, "#kill -9 "+pid);*/
+
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        String packageName = null;
+        try {
+            if (processName.indexOf(":") == -1) {
+                packageName = processName;
+            } else {
+                packageName = processName.split(":")[0];
+            }
+
+            activityManager.killBackgroundProcesses(packageName);
+
+            //
+            Method forceStopPackage = activityManager.getClass().getDeclaredMethod("forceStopPackage", String.class);
+            forceStopPackage.setAccessible(true);
+            forceStopPackage.invoke(activityManager, packageName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 描述：获取可用内存.
+     *
+     * @param context
+     * @return
+     */
+    public static long getAvailMemory(Context context) {
+        //获取android当前可用内存大小
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
+        activityManager.getMemoryInfo(memoryInfo);
+        //当前系统可用内存 ,将获得的内存大小规格化
+        return memoryInfo.availMem;
+    }
+
+    /**
+     * 描述：总内存.
+     *
+     * @param context
+     * @return
+     */
+    public static long getTotalMemory(Context context) {
+        //系统内存信息文件
+        String file = "/proc/meminfo";
+        String memInfo;
+        String[] strs;
+        long memory = 0;
+
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader, 8192);
+            //读取meminfo第一行，系统内存大小
+            memInfo = bufferedReader.readLine();
+            strs = memInfo.split("\\s+");
+            for (String str : strs) {
+                Log.d(TAG, str + "\t");
+            }
+            //获得系统总内存，单位KB
+            memory = Integer.valueOf(strs[1]).intValue() * 1024;
+            bufferedReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //Byte转位KB或MB
+        return memory;
     }
 }
